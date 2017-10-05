4141ea23469af6b810b59934b8f6869f6316f9ba
Wojciech Nadurski <wojciech.nadurski@oke.pl>
2017-10-05 13:47:47 +0200

    Adding report style to env constants

--- constants/index.js
+++ constants/index.js
@@ -3,23 +3,24 @@
 const path = require('path');
 
 module.exports = {
-  ENVIRONMENT_CONFIG_MAP: {
-    JEST_SUITE_NAME: 'suiteName',
-    JEST_JUNIT_OUTPUT: 'output',
-    JEST_JUNIT_CLASSNAME: 'classNameTemplate',
-    JEST_JUNIT_TITLE: 'titleTemplate',
-    JEST_JUNIT_ANCESTOR_SEPARATOR: 'ancestorSeparator',
-    JEST_USE_PATH_FOR_SUITE_NAME: 'usePathForSuiteName',
-  },
-  DEFAULT_OPTIONS: {
-    suiteName: 'jest tests',
-    output: path.join(process.cwd(), './jest-tests-report.xml'),
-    classNameTemplate: '{classname} {title}',
-    titleTemplate: '{classname} {title}',
-    ancestorSeparator: ' ',
-    usePathForSuiteName: 'false',
-    reportStyle: "junit"
-  },
-  CLASSNAME_VAR: '{classname}',
-  TITLE_VAR: '{title}',
+    ENVIRONMENT_CONFIG_MAP: {
+        JEST_SUITE_NAME: 'suiteName',
+        JEST_JUNIT_OUTPUT: 'output',
+        JEST_JUNIT_CLASSNAME: 'classNameTemplate',
+        JEST_JUNIT_TITLE: 'titleTemplate',
+        JEST_JUNIT_ANCESTOR_SEPARATOR: 'ancestorSeparator',
+        JEST_USE_PATH_FOR_SUITE_NAME: 'usePathForSuiteName',
+        JEST_REPORT_STYLE: "reportStyle"
+    },
+    DEFAULT_OPTIONS: {
+        suiteName: 'jest tests',
+        output: path.join(process.cwd(), './jest-tests-report.xml'),
+        classNameTemplate: '{classname} {title}',
+        titleTemplate: '{classname} {title}',
+        ancestorSeparator: ' ',
+        usePathForSuiteName: 'false',
+        reportStyle: "junit"
+    },
+    CLASSNAME_VAR: '{classname}',
+    TITLE_VAR: '{title}',
 };
